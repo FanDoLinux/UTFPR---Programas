eb8afe27049380ff4cca9889a1158b59504026b6
FanDoLinux <lcboaretto@hotmail.com>
2023-10-09 10:18:51 -0300

    Upload commit

---
+++ Algoritimos e Programação/Lista 2 - Estrutura de Repetição/exercicio 10.c
@@ -0,0 +1,11 @@
+/*Faça o programa que apresente a seguinte saída como no exemplo, perguntando ao usuário o número
+máximo (deve ser positivo).*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+
+    return 0;
+}
---
+++ Algoritimos e Programação/Lista 2 - Estrutura de Repetição/exercicio 9.c
@@ -0,0 +1,10 @@
+/*Faça o programa que apresente a seguinte saída como no exemplo, solicitando ao usuário o número
+máximo (deve ser positivo).*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
